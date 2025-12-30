13ebc29524cdec4a0bc260ca37447aeb3067e2de
Gods'time <igegodstime0@gmail.com>
2025-12-30 05:07:59 +0000

    added parallax scroll

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -2,15 +2,15 @@ export default function Experience() {
   return (
     <div className="experience-section" id="technologies">
       <h1>Experience with</h1>
-      <div>
-        <img src="./assets/images/Group 12.svg" alt="" className="experience-logos" />
-        <img src="./assets/images/Group 13.svg" alt="" className="experience-logos" />
-        <img src="./assets/images/Group 14.svg" alt="" className="experience-logos" />
-        <img src="./assets/images/Group.svg" alt="" className="experience-logos" />
-        <img src="./assets/images/akar-icons_github-fill.svg" alt="" className="experience-logos" />
-        <img src="./assets/images/vscode-icons_file-type-tailwind.svg" alt="" className="experience-logos" />
-        <img src="./assets/images/vscode-icons_file-type-vscode.svg" alt="" className="experience-logos"/>
-        <img src="./assets/images/git.svg" alt="" className="experience-logos"/>
+      <div className="experience-logos-container">
+        <img src="./assets/images/Group 12.svg" alt="tool-1" className="experience-logos" />
+        <img src="./assets/images/Group 13.svg" alt="tool-2" className="experience-logos" />
+        <img src="./assets/images/Group 14.svg" alt="tool-3" className="experience-logos" />
+        <img src="./assets/images/Group.svg" alt="tool-4" className="experience-logos" />
+        <img src="./assets/images/akar-icons_github-fill.svg" alt="github" className="experience-logos" />
+        <img src="./assets/images/vscode-icons_file-type-tailwind.svg" alt="tailwind" className="experience-logos" />
+        <img src="./assets/images/vscode-icons_file-type-vscode.svg" alt="vscode" className="experience-logos"/>
+        <img src="./assets/images/git.svg" alt="git" className="experience-logos"/>
       
       </div>
     </div>
